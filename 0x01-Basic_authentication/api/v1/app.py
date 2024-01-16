40f017ec007b6083e3d8b7216a38fbf3a8ab3d05
AlvyneZ <alvynew@gmail.com>
2024-01-16 21:38:43 +0300

    Basic Auth class

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -5,6 +5,7 @@ Route module for the API
 from os import getenv
 from api.v1.views import app_views
 from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
@@ -59,6 +60,9 @@ def authenticate():
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    if (getenv("API_AUTH") == "auth"):
+    auth_type = getenv("API_AUTH", "auth")
+    if auth_type == "auth":
         auth = Auth()
+    elif auth_type == "basic_auth":
+        buth = BasicAuth()
     app.run(host=host, port=port)
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Provides a Basic Authentication class
+"""
+from .auth import Auth
+
+
+class BasicAuth(Auth):
+    """Provides basic authentication services
+    """
