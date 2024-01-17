596f17c31e63e3936bb0d165e1ec5ace0dfbe1a7
AlvyneZ <alvynew@gmail.com>
2024-01-17 20:18:40 +0300

    Getting User from Session cookie of request

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,8 +2,10 @@
 """ Provides a Session Authentication class
 """
 from uuid import uuid4
+from typing import TypeVar
 
 from .auth import Auth
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -38,3 +40,10 @@ class SessionAuth(Auth):
         if (session_id is None) or (type(session_id) != str):
             return None
         return self.user_id_by_session_id.get(session_id, None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Retrieves the user associated with the request.
+        """
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        return User.get(user_id)
