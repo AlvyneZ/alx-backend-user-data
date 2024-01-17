1a7c1fb79b42efd207927a692e44ad853a07271f
AlvyneZ <alvynew@gmail.com>
2024-01-17 19:33:54 +0300

    Placeholder SessionAuth class

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Provides a Session Authentication class
+"""
+from .auth import Auth
+
+
+class SessionAuth(Auth):
+    """Provides session authentication services
+    """
