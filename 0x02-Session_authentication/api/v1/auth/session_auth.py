f6288bd387bac2659a26d1f2cc88f4c3fcfc281c
AlvyneZ <alvynew@gmail.com>
2024-01-17 19:48:40 +0300

    Creating in memory session storage

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,9 +1,27 @@
 #!/usr/bin/env python3
 """ Provides a Session Authentication class
 """
+from uuid import uuid4
+
 from .auth import Auth
 
 
 class SessionAuth(Auth):
     """Provides session authentication services
     """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a session ID for a given User ID
+
+        Args:
+            user_id (str, optional): _description_. Defaults to None.
+
+        Returns:
+            str: _description_
+        """
+        if (user_id is None) or (type(user_id) != str):
+            return None
+        session_id = str(uuid4())
+        self.user_id_by_session_id.update({session_id: user_id})
+        return session_id
