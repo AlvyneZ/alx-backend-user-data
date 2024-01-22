da89f7f086b3a22110645c43d59375d07771a7a5
AlvyneZ <alvynew@gmail.com>
2024-01-22 20:35:49 +0300

    Adding User to sqlite database

---
+++ 0x03-user_authentication_service/test.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+This "test.py" file provides code for testing
+ the code implemented in this project
+"""
+
+"""
+Testing the sqlAlchemy User Model
+"""
+from user import User
+
+
+print(User.__tablename__)
+
+for column in User.__table__.columns:
+    print("{}: {}".format(column, column.type))
+
+print("====================")
+
+"""
+Testing the add_user method in the db
+"""
+from db import DB
+from user import User
+
+my_db = DB()
+
+user_1 = my_db.add_user("test@test.com", "SuperHashedPwd")
+print(user_1.id)
+
+user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
+print(user_2.id)
+
+print("====================")
